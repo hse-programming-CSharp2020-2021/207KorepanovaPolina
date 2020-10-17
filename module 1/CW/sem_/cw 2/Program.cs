fb6e5d433b0c3a38b9356595543fe499b19481f4
polinnna <70817845+polinnna@users.noreply.github.com>
2020-10-17 13:30:22 +0300

    Add files via upload

---
+++ module 1/CW/sem_/cw 2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cw_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string readFromFile1 = File.ReadAllText(path);
+        }
+    }
+}
