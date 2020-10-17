0822e5b67380e7aaa1f80342ca78acf7e4bb9d71
polinnna <70817845+polinnna@users.noreply.github.com>
2020-10-17 13:27:39 +0300

    Add files via upload

---
+++ module 1/CW/sem_/cw 1/cw 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace cw_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stri = "";
+            string s = Console.ReadLine();
+            int nm = int.Parse(s);
+             while (nm != 0)
+            {
+                stri = (nm % 2).ToString() + stri;
+                nm /= 2;
+            }
+         
+            
+            
+            File.WriteAllText("IntNumber.txt", stri);
+
+        }
+    }
+}
