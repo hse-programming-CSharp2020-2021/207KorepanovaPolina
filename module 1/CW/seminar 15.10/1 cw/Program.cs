f7f3f9f93bd3306ec3520ba064995b166506fed0
polinnna <70817845+polinnna@users.noreply.github.com>
2020-10-17 13:43:03 +0300

    Add files via upload

---
+++ module 1/CW/seminar 15.10/1 cw/Program.cs
@@ -0,0 +1,65 @@
+﻿//[7:49 PM] Горденко Мария Константиновна
+using System;
+
+using System.IO; namespace Task06
+
+{​​​​
+
+    class Program
+
+    {​​​​
+
+        static void Main(string[] args)
+
+        {​​​​
+
+            try
+
+            {​​​​
+
+                int n = int.Parse(Console.ReadLine());
+
+            }​​​​
+
+
+      catch (FormatException ex)
+
+            {​​​​
+
+                Console.WriteLine("format!" + ex.Message);
+
+            }​​​​
+
+
+      catch (OverflowException ex)
+
+            {​​​​
+
+                Console.WriteLine("over!" + ex.Message);
+
+            }​​​​
+
+
+      catch (Exception ex)
+
+            {​​​​
+
+                Console.WriteLine("ex!" + ex.Message);
+
+            }​​​​
+
+
+      finally
+
+            {​​​​
+
+                Console.WriteLine("fin!");
+
+            }​​​​
+
+        }​​​​
+
+    }​​​​
+
+}​​​​
+
---
+++ module 1/CW/seminar 15.10/2/Program.cs
@@ -0,0 +1,27 @@
+﻿//[7:54 PM] Горденко Мария Константиновна
+using System;
+    // Для доступа к классу Consoleusing System.IO; 
+    // Для работы с файлами и директориями
+    namespace Directories 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Блок try-catch при работе с файлами обязателен!            
+            try
+            {
+                DirectoryOverview(@"..\..\..\");
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+                Console.WriteLine("Нажмите любую клавишу, чтобы выйти");
+            Console.ReadLine();
+        }
+    }
+}
+
